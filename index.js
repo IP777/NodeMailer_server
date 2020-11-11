0139e894b47912d404490dccf00c7645a0cc98e6
IP777 <ivanov_piter@ukr.net>
2020-11-11 19:54:37 +0200

    add cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const dotenv = require("dotenv");
 const controller = require("./api/send.controller");
@@ -9,7 +10,9 @@ const PORT = process.env.PORT || 80;
 //Прослойка для обработки запросов от json
 app.use(express.json());
 //Прослойка для обработки запросов от формы
-//app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+// CORS заголовки с разрешением доступа с любого сервера
+app.use(cors());
 
 app.post("/mail/send/:user", controller.sendMail);
 app.get("/test", controller.testServer);

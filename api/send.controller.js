a29db7e28c581783c9b19c20118f09543e84d905
IP777 <ivanov_piter@ukr.net>
2020-11-11 12:14:59 +0200

    make it better

---
+++ api/send.controller.js
@@ -0,0 +1,40 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const { MAIL_LOGIN, MAIL_PASS, PITER_MAIL, VADOS_MAIL } = process.env;
+
+//Использован пакет Nodemailer
+//https://nodemailer.com/about/
+
+async function sendMail(req, res, next) {
+	try {
+		const { name, email, message } = req.body;
+
+		let transporter = nodemailer.createTransport({
+			host: "smtp.gmail.com",
+			port: 465,
+			secure: true, // true for 465, false for other ports
+			auth: {
+				user: MAIL_LOGIN, // generated ethereal user
+				pass: MAIL_PASS, // generated ethereal password
+			},
+		});
+
+		// send mail with defined transport object
+		let info = await transporter.sendMail({
+			from: `${email} <foo@example.com>`, // sender address
+			to: PITER_MAIL, // list of receivers
+			subject: "Оповешение с сайта резюме.", // Subject line
+			//text: "Hello world--?", // plain text body
+			html: `<b>${name} пишет:</b><br><p>${message}</p>`, // html body
+		});
+
+		res.status(200).send(info.messageId);
+	} catch (err) {
+		next(err);
+	}
+}
+
+module.exports = {
+	sendMail,
+};
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const controller = require("./api/send.controller");
+
+dotenv.config();
+const PORT = process.env.PORT || 80;
+
+//Прослойка для обработки запросов от json
+app.use(express.json());
+//Прослойка для обработки запросов от формы
+//app.use(express.urlencoded({ extended: true }));
+
+app.post("/mail/send", controller.sendMail);
+
+app.listen(PORT, () => {
+	console.log(`Server has been started ${PORT}...`);
+});
